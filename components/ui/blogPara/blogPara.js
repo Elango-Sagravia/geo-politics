0ed0e682a5f71fb13a93faf248c2178ef4f7f2ef
Elango Palanisamy <elango@Elangos-Mac-mini.local>
2024-08-06 22:30:50 +0530

    removed everything

--- components/ui/blogPara/blogPara.js
+++
@@ -1,7 +0,0 @@
-export default function BlogPara({ className, children }) {
-  return (
-    <p className={`text-sm py-4 leading-loose text-black ${className}`}>
-      {children}
-    </p>
-  );
-}
--- components/ui/blogTitle/blogTitle.js
+++
@@ -1,3 +0,0 @@
-export default function BlogTitle({ title }) {
-  return <h1 className="text-4xl w-5/6 text-nl_background">{title}</h1>;
-}
