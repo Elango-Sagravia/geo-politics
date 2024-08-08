978afbc8d6c779bed184ebe3222a21606c2f507a
Elango Palanisamy <elango@Elangos-Mac-mini.local>
2024-08-08 18:34:33 +0530

    added images

--- blogs.js
+++ blogs.js
@@ -1,52 +1,64 @@
 const blogs = [
   {
-    slug: "first-blog",
-    title: "First Blog",
-    published_at: "4 Jul 2024",
+    slug: "why-is-venezuela-so-poor-despite-having-the-worlds-largest-crude-oil-reserves",
+    title:
+      "Why is Venezuela So Poor Despite Having the World's Largest Crude Oil Reserves?",
+    published_at: "8 Aug 2024",
     author: "Helen Lewis",
     author_image: "https://github.com/shadcn.png",
-    read_time: "9 MIN",
+    read_time: "4 MIN",
     banner_image:
       "https://hedonova.b-cdn.net/Hedonova%20images/%20All%20Hedonova%20blogs/How%20to%20calculate%20your%20retirement%20needs/How%20to%20calculate%20your%20retirement%20needs.webp",
-    cut_off: 3,
+    cut_off: 5,
     related_articles: ["second-blog"],
     data: [
       {
         classname: "",
-        type: "p",
+        type: "h3",
         content:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      },
-      { classname: "", type: "h3", content: "Another Heading" },
-      {
-        classname: "",
-        type: "p",
-        content:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          "Despite having the largest oil reserves, why is Venezuela's oil production so low?",
       },
       {
         classname: "",
-        type: "p",
-        content:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        type: "img",
+        link: "/blogs/blogImages/venezula-blog/venezula_blog_1.png",
+        alt: "Venezula oil production details",
       },
       {
         classname: "",
         type: "p",
         content:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          "Venezuela had the world's largest proven crude oil reserves in 2023, with approximately 303 billion barrels, accounting for about 17% of global reserves. The country produced only 0.8% of global crude oil in 2023 despite this. The bulk of Venezuelan crude oil is of lower quality, often heavy and unsatisfactory for lubricant manufacture without extensive solvent treating processes. Mismanagement and underinvestment in oil infrastructure further hinder production capacity, preventing Venezuela from fully capitalizing on its vast reserves.",
+      },
+      {
+        classname: "",
+        type: "h3",
+        content:
+          "How has political instability impacted Venezuela's economic condition?",
       },
       {
         classname: "",
         type: "p",
         content:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          "Political instability, characterized by authoritarian governance, frequent changes in leadership, and inconsistent economic policies, has eroded investor confidence and deterred foreign investment. This instability has led to economic mismanagement, exacerbating Venezuela's financial woes and deepening its economic crisis.",
+      },
+      {
+        classname: "",
+        type: "h3",
+        content:
+          "What role has corruption played in Venezuela's economic decline?",
+      },
+      {
+        classname: "",
+        type: "img",
+        link: "/blogs/blogImages/venezula-blog/venezula_blog_2.png",
+        alt: "Venezula corruption details",
       },
       {
         classname: "",
         type: "p",
         content:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          "Rampant corruption has diverted public funds from essential services and infrastructure into private hands, undermining economic development. The nationalization of the oil industry and mismanagement by the state-owned company, Petroleos de Venezuela, resulted in inefficiencies and a drastic decline in oil production, further worsening the economic situation and contributing to widespread poverty.",
       },
     ],
   },
