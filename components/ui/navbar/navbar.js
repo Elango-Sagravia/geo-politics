14ac67661cb6ea88e6f2ea979da7639ad2910a59
Elango Palanisamy <elango@Elangos-Mac-mini.local>
2024-08-08 15:33:03 +0530

    added logo

--- components/ui/navbar/navbar.js
+++ components/ui/navbar/navbar.js
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 
 import { usePathname } from "next/navigation";
@@ -12,15 +13,18 @@ export default function Navbar() {
   const pathname = usePathname();
   return (
     <header className="sticky top-0 bg-nl_sec_background px-4 md:px-16 py-6 flex justify-between items-center border-b border-nl_button_border z-10">
-      <Link href="/" className="text-2xl text-black">
-        Logotype
+      <Link href="/">
+        <div className="w-32 md:w-48">
+          <img src="/logo.png" layout="responsive" alt="Logo" />
+        </div>
       </Link>
-      <nav className="text-base">
+
+      <nav className="text-[12px] sm:text-base">
         {routes.map((route) => (
           <Link
             key={route.path}
             href={route.path}
-            className={`ml-6 text-black ${
+            className={`ml-2 md:ml-6 text-black ${
               pathname === route.path && "underline underline-offset-4"
             }`}
           >
