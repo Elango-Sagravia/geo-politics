b5befb983d84127dabb1731d11e62204fcdc1d90
Elango-Sagravia <elango@sagravia.com>
2024-12-27 21:23:44 +0530

    fixed seo issue

--- app/(web)/policy/[slug]/page.js
+++ app/(web)/policy/[slug]/page.js
@@ -41,7 +41,9 @@ export async function generateMetadata({ params }) {
   const { slug } = params;
 
   // Fetch metadata for the slug
-  const post = getDocumentBySlug("policies", slug, ["title"]);
+  const post = getDocumentBySlug("policies", slug, [
+    "seoTitle, seoDescription",
+  ]);
 
   return {
     title: post.title,
