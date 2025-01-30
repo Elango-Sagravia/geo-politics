426214d1ab621ad8fcd1a22ae7edc1e8c0d4f222
Elango-Sagravia <elango@sagravia.com>
2025-01-30 19:14:48 +0530

    removed users post

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -25,51 +25,51 @@ export async function GET() {
 }
 
 // POST API - Update zbStatus and zbSubStatus
-export async function POST(request) {
-  try {
-    const { uniqueid, zbStatus, zbSubStatus } = await request.json();
+// export async function POST(request) {
+//   try {
+//     const { uniqueid, zbStatus, zbSubStatus } = await request.json();
 
-    console.log(uniqueid, zbStatus, zbSubStatus);
+//     console.log(uniqueid, zbStatus, zbSubStatus);
 
-    if (!uniqueid || !zbStatus) {
-      return new Response(
-        JSON.stringify({ success: false, message: "Missing required fields" }),
-        { status: 400 }
-      );
-    }
+//     if (!uniqueid || !zbStatus) {
+//       return new Response(
+//         JSON.stringify({ success: false, message: "Missing required fields" }),
+//         { status: 400 }
+//       );
+//     }
 
-    const updateUserQuery = `
-      UPDATE users
-      SET zbStatus = $1, zbSubStatus = $2, updated_at = NOW()
-      WHERE uniqueid = $3
-      RETURNING id, email, zbStatus, zbSubStatus;
-    `;
+//     const updateUserQuery = `
+//       UPDATE users
+//       SET zbStatus = $1, zbSubStatus = $2, updated_at = NOW()
+//       WHERE uniqueid = $3
+//       RETURNING id, email, zbStatus, zbSubStatus;
+//     `;
 
-    const result = await query(updateUserQuery, [
-      zbStatus,
-      zbSubStatus,
-      uniqueid,
-    ]);
+//     const result = await query(updateUserQuery, [
+//       zbStatus,
+//       zbSubStatus,
+//       uniqueid,
+//     ]);
 
-    if (result.rows.length === 0) {
-      return new Response(
-        JSON.stringify({ success: false, message: "User not found" }),
-        { status: 404 }
-      );
-    }
+//     if (result.rows.length === 0) {
+//       return new Response(
+//         JSON.stringify({ success: false, message: "User not found" }),
+//         { status: 404 }
+//       );
+//     }
 
-    return new Response(
-      JSON.stringify({ success: true, user: result.rows[0] }),
-      {
-        status: 200,
-        headers: { "Content-Type": "application/json" },
-      }
-    );
-  } catch (error) {
-    console.error("Error updating user:", error);
-    return new Response(
-      JSON.stringify({ success: false, error: error.message }),
-      { status: 500 }
-    );
-  }
-}
+//     return new Response(
+//       JSON.stringify({ success: true, user: result.rows[0] }),
+//       {
+//         status: 200,
+//         headers: { "Content-Type": "application/json" },
+//       }
+//     );
+//   } catch (error) {
+//     console.error("Error updating user:", error);
+//     return new Response(
+//       JSON.stringify({ success: false, error: error.message }),
+//       { status: 500 }
+//     );
+//   }
+// }
