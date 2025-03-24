acf15e994a1b9d2bbbbab77b181bff33eea8e0a6
Elango-Sagravia <elango@sagravia.com>
2025-03-24 19:57:57 +0530

    added gtm

--- app/(web)/layout.js
+++ app/(web)/layout.js
@@ -123,6 +123,28 @@ export default function RootLayout({ children }) {
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "qd9zb94wyc");`}
         </Script>
+        <Script id="gm" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K72D66BW');`}
+        </Script>
+        <Script
+          id="gtm-noscript"
+          dangerouslySetInnerHTML={{
+            __html: `
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-K72D66BW"
+          height="0"
+          width="0"
+          style="display:none;visibility:hidden"
+        ></iframe>
+      </noscript>
+    `,
+          }}
+        />
       </head>
       <body>
         <ThemeProvider
